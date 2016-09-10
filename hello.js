5bc01c716b2e84f05ab5d6dbaa08dced9cf44e36
Tomvictor <vjtomvictor@icloud.com>
2016-09-10 23:33:47 +0530

    learning

--- hello.js
+++ hello.js
@@ -1,2 +1,7 @@
 console.log("hello world");
 console.log("welcome to node.js by Tomvictor")
+
+
+var hello = "hai tomvicto,wecome to nodejs";
+justNode = hello.slice(10);
+console.log(`${justNode}`)
