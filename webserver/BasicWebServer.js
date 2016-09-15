451c47c52f04506b5fcc145462b0f5b45964c442
Tomvictor <vjtomvictor@icloud.com>
2016-09-15 20:23:26 +0530

    mime added

--- webserver/BasicWebServer.js
+++ webserver/BasicWebServer.js
@@ -1,10 +1,22 @@
 var http =require('http'),
 	host = '127.0.0.1',
-	port = '9000'
+	fs = require('fs'),
+	path = require('path'),
+	port = '9000';
+
+var mimes = {
+	".htm" : "text/html",
+	".css" : "text/css",
+	".js" : "text/javascript",
+	".gif": "image/gif",
+	".jpg" : "image/jpeg",
+	".png" : "image/png"
+	}
 
 var server = http.createServer(function(req, res){
-	res.writeHead(200,{'content-Type' : 'text/html'})
-	res.end('<h1>Hello world ! </h1>');
+	var filepath = (req.url === '/') ? ('./index.htm'): ('.' + req.url);
+	// check to see if the file eists
+
 }).listen(port,host,function () {
 	// body...
 	console.log('server Running on http://' + host + ':' + port);
