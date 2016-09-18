a0174bd3c19b578ec0d941cfe1f92a84e51e1011
Tomvictor <vjtomvictor@icloud.com>
2016-09-18 23:29:17 +0530

    installed hogan template engine
    and created new express view

--- chat app/app.js
+++ chat app/app.js
@@ -1,9 +1,17 @@
+ 
  var express = require('express'),
- 	app = express();
+ 	app = express(),
+ 	path = require('path')
+
+ app.set('views',path.join(__dirname,'views'));
+ app.engine('html',require('hogan-express'));
+ app.set('view engine','html');
+ app.use(express.static(path.join(__dirname,'public')));
 
  app.route('/').get(function (req,res,next) {
  	// body...
- 	res.send('<h1>hello world</h1>');
+ 	// res.send('<h1>hello world</h1>');
+ 	res.render('index');
  })
 
  app.listen(3000,function () {
