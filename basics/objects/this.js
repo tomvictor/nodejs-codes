21ea08822dc6f55d990c726943ed23422c8f2412
Tomvictor <vjtomvictor@icloud.com>
2016-09-21 19:38:05 +0530

    prototype example working fine

--- basics/objects/this.js
+++ basics/objects/this.js
@@ -1,13 +1,14 @@
 
 function Person(firstname, lastname) {
-	// body...
 	this.firstname = firstname ;
 	this.lastname = lastname ; 
 }
 
-Person.prototype.greet = function () {
-	console.log('hello' + this.firstname + ' ' + this.lastname);
+Person.prototype.greet = function() {
+	console.log('hello' + ' ' + this.firstname + ' ' + this.lastname);
 };
 
 var tom = new Person('tom', 'victor');
 console.log(tom.firstname);
+
+tom.greet();
