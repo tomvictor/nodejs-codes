9d9354a44771e736bb87e5e97d469d552fd4f826
Tom Victor <vjtomvictor@icloud.com>
2016-10-08 23:30:38 +0530

    Array of functions and arr.push method

--- events and event emitters/app.js
+++ events and event emitters/app.js
@@ -11,3 +11,29 @@ console.log(obj["greet"]);
 var prop = "greet";
 
 console.log(obj[prop]) ;
+
+// functions and arrays
+
+var arr = [] ;
+
+arr.push(function () {
+	console.log("hello world 1")
+});
+arr.push(function () {
+	console.log("hello world 2")
+});
+arr.push(function () {
+	console.log("hello world 3")
+});
+
+
+arr.forEach(function (item) {
+	item();
+});
+
+
+
+
+
+
+
