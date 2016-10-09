d1d2ccb85441b31a2f526b69c154403cfae2854f
Tom Victor <vjtomvictor@icloud.com>
2016-10-09 13:07:17 +0530

    Prototype working now
    only examples, while learning

--- prototype.js
+++ prototype.js
@@ -1,6 +1,16 @@
 function Person(firstname, lastname){
 	this.firstname = firstname;
 	this.lastname = lastname ;
+};
+
+Person.prototype.greet = function () {
+	console.log('hello' + this.firstname + ' '
+		+ this.lastname)
 }
 
 var john = new Person('tom', 'victor') ; 
+console.log(john.firstname);
+
+//prottype calling
+
+john.greet();
