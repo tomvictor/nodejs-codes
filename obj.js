7a434eebd355486fa4ec01b67baae6e64670b012
Tom Victor <vjtomvictor@icloud.com>
2016-10-09 12:39:36 +0530

    Javascript obj example

---
+++ obj.js
@@ -0,0 +1,11 @@
+// javascript object example
+var person ={
+	firtname : 'john',
+	lastname : 'Doe' ,
+	greet : function () {
+		console.log('hello' + 
+			this.firtname + ' ' + 
+			this.lastname);
+	}
+};
+
