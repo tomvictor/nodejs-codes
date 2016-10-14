579f1ecd47bed15484441c50be119b4976ead418
Tom Victor <vjtomvictor@icloud.com>
2016-10-14 19:01:51 +0530

    Now working properly

--- server/app.js
+++ server/app.js
@@ -4,5 +4,7 @@ var http = require('http');
 
 http.createServer(function (req,res) {
 	// function body
-	
-})
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello');
+
+}).listen(1337,'127.0.0.1')
