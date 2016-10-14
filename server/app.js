f6622f26c455171e663bdcf4730c19862d2e9a3c
Tom Victor <vjtomvictor@icloud.com>
2016-10-14 19:12:10 +0530

    Nodejs is outputting html template

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 // my simple code for a nodejs server
 
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req,res) {
 	// function body
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello');
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var html = fs.readFileSync(__dirname + '/index.html')
+	res.end(html);
 
 }).listen(1337,'127.0.0.1')
 
