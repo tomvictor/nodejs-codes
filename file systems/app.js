bc6997aa9cbce60b7bee90326321414a87210bf1
Tom Victor <vjtomvictor@icloud.com>
2016-10-14 18:56:39 +0530

    Progress codeing

--- file systems/app.js
+++ file systems/app.js
@@ -3,3 +3,5 @@ var fs = require('fs');
 var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
 
 console.log(greet);
+
+
---
+++ server/app.js
@@ -0,0 +1,8 @@
+// my simple code for a nodejs server
+
+var http = require('http');
+
+http.createServer(function (req,res) {
+	// function body
+	
+})
